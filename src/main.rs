941cba7036923c1dddd98f4c760080b0b91e112a
Alexis JAPAS <alexis.japas.dev@pm.me>
2025-04-06 16:25:17 +0200

    feat: hash using sha2(256)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use sha2::{Sha256, Digest};
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut hasher = Sha256::new();
+    let message = "Hello, world!";
+    
+    hasher.update(message.as_bytes());
+    let result = hasher.finalize();
+    
+    println!("{} => {:x}", message, result);
 }
