98489349567c511db89393c61cc8da06485a40be
Alexis JAPAS <alexis.japas.dev@pm.me>
2025-04-22 10:43:26 +0200

    feat: properly add transactions to pools

--- src/main.rs
+++ src/main.rs
@@ -27,24 +27,50 @@ fn main() {
     let bob_signing_key = SigningKey::random(&mut OsRng);
     let bob_verifying_key = VerifyingKey::from(&bob_signing_key);
 
-    // TRANSACTIONS
-    // Alice to Bob
+    // TRANSACTIONS: batch 1
+    // Generate & sign transactions
     let mut t_0 = cairn::Transaction::new(alice_verifying_key, bob_verifying_key, 1);
     t_0.sign(&alice_signing_key);
     let mut t_1 = cairn::Transaction::new(alice_verifying_key, bob_verifying_key, 3);
     t_1.sign(&alice_signing_key);
+    let mut t_2 = cairn::Transaction::new(alice_verifying_key, bob_verifying_key, 10);
+    t_2.sign(&alice_signing_key);
+    let mut t_3 = cairn::Transaction::new(bob_verifying_key, alice_verifying_key, 8);
+    t_3.sign(&bob_signing_key);
+    
+    // Send transactions
     tx_alice.send(cairn::Message::NewTransaction(t_0.clone())).unwrap();
-    tx_alice.send(cairn::Message::NewTransaction(t_0.clone())).unwrap();
+    tx_alice.send(cairn::Message::NewTransaction(t_1.clone())).unwrap();
 
     // Read
     loop {
         match rx.try_recv() {
             Ok(message) => {
+                // Alice node
                 match message {
                     cairn::Message::NewTransaction(transaction) => {
                         if transaction.verify() {
-                            println!("Verified transaction: {:?}", transaction);
-                            node_bob_tx_pool.insert(transaction.hash(), transaction);
+                            println!("Verified transaction:\n{:?}", transaction);
+                            let entry = node_bob_tx_pool.entry(transaction.hash());
+                            match entry {
+                                std::collections::hash_map::Entry::Vacant(e) => {
+                                    e.insert(transaction.clone());
+                                    println!("Transaction inserted.");
+                                },
+                                std::collections::hash_map::Entry::Occupied(_) => {
+                                    println!("Transaction already exists.");
+                                }
+                            }
+                            let entry = node_bob_tx_pool.entry(transaction.hash());
+                            match entry {
+                                std::collections::hash_map::Entry::Vacant(e) => {
+                                    e.insert(transaction.clone());
+                                    println!("Transaction inserted.");
+                                },
+                                std::collections::hash_map::Entry::Occupied(_) => {
+                                    println!("Transaction already exists.");
+                                }
+                            }
                         } else {
                             println!("Transaction verification failed: {:?}", transaction);
                         }
@@ -65,14 +91,11 @@ fn main() {
         }
     }
 
-    // Other transactions to add to the block
-    
-    let mut t_2 = cairn::Transaction::new(alice_verifying_key, bob_verifying_key, 10);
-    t_2.sign(&alice_signing_key);
-    let mut t_3 = cairn::Transaction::new(bob_verifying_key, alice_verifying_key, 8);
-    t_3.sign(&bob_signing_key);
+    // TRANSACTIONS: batch 2
     let mut t_4 = cairn::Transaction::new(bob_verifying_key, alice_verifying_key, 1);
     t_4.sign(&bob_signing_key);
     let mut t_5 = cairn::Transaction::new(bob_verifying_key, alice_verifying_key, 2);
     t_5.sign(&bob_signing_key);
+    
+    
 }
